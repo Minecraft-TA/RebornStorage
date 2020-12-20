4d50de33e303523d71be2c9f8640520b7c341aae
tth05 <36999320+tth05@users.noreply.github.com>
2020-12-20 20:25:54 +0100

    v3.3.5.2

--- src/main/java/me/modmuss50/rebornstorage/lib/ModInfo.java
+++ src/main/java/me/modmuss50/rebornstorage/lib/ModInfo.java
@@ -7,7 +7,7 @@ public class ModInfo {
 	public static final String MOD_ID = "rebornstorage";
 	public static final String MOD_NAME = "Reborn Storage";
 	public static final String MOD_VERSION = "@VERSION@";
-	public static final String MOD_DEPENDENCIES = "required-after:reborncore;required-after:refinedstorage@[2.2.2,);";
+	public static final String MOD_DEPENDENCIES = "required-after:reborncore;required-after:refinedstorage@[2.2.4,);";
 	public static final String COMMON_PROXY_LOC = "me.modmuss50.rebornstorage.proxys.CommonProxy";
 	public static final String CLIENT_PROXY_LOC = "me.modmuss50.rebornstorage.proxys.ClientProxy";
 }
